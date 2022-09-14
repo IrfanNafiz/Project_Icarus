81ffbb9af0a809cfc7d0ee0459365a6e6c3168cf
Irfan Nafiz Shahan <irfannafizislive@gmail.com>
2022-09-14 23:10:59 +0600

    Uploaded Project

---
+++ Space Game Prototype/Assets/StellarBody.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Rigidbody))]
+public class StellarBody : GravityXp
+{
+    public static float gravConstant = 6.67384e-11f;
+
+    public Rigidbody rb;
+    public float radius;
+    public Vector3 initialVelocity;
+    public string bodyName = "Unnamed";
+    public float surfaceGravity;
+    Transform meshHolder;
+    public float mass { get; private set; }
+    public Vector3 currentVelocity { get; private set; }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentVelocity = initialVelocity;
+        rb.mass = mass;
+    }
+
+    public void UpdateVelocity(StellarBody[] allBodies, float timeStep)
+    {
+        foreach (var otherBody in allBodies)
+        {
+            if (otherBody != this)
+            {
+                float sqrDist = (otherBody.rb.position - rb.position).sqrMagnitude;
+                Vector3 forceDir = (otherBody.rb.position - rb.position).normalized;
+                Vector3 gravitationalAcceleration = forceDir * gravConstant * otherBody.mass / sqrDist;
+                currentVelocity += gravitationalAcceleration * timeStep;
+            };
+        };
+    }
+
+    public void UpdateVelocity(Vector3 gravitationalAcceleration, float timeStep)
+    {
+        currentVelocity += gravitationalAcceleration * timeStep;
+    }
+
+    public void UpdatePosition(float timeStep)
+    {
+        rb.MovePosition(rb.position + currentVelocity * timeStep);
+    }
+
+    void OnValidate() //Understand what this means and decide on what to do, 
+                        //it caused the clouds to transform scale according to radius set when played (OnValidate effect)
+    {
+        mass = surfaceGravity * radius * radius / gravConstant;
+        //meshHolder = transform.GetChild(1);
+        //meshHolder.localScale = Vector3.one * radius;
+        //meshHolder = transform.GetChild(0);
+        //meshHolder.localScale = Vector3.one * radius;
+        gameObject.name = bodyName;
+    }
+
+    public Rigidbody Rigidbody
+    {
+        get
+        {
+            return rb;
+        }
+    }
+
+    public Vector3 Position
+    {
+        get
+        {
+            return rb.position;
+        }
+    }
+}
