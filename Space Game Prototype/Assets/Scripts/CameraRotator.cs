ec573959233023ad4b47d5409743bef1409b8d66
Irfan Nafiz Shahan <irfannafizislive@gmail.com>
2022-09-26 21:57:28 +0600

    Added Solar System Trajectory Scene

---
+++ Space Game Prototype/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{ 
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+//        transform.Rotate(0, speed * Time.deltatime, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
