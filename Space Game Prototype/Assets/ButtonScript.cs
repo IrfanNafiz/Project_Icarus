fb574a5408ffee97998da3365108362d01c98aca
Arban Hossain <arbanhossain@gmail.com>
2022-09-30 02:07:30 +0600

    flyby done

---
+++ Space Game Prototype/Assets/ButtonScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonScript : MonoBehaviour
+{
+    GameObject child;
+    private bool showing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        child = transform.GetChild(0).gameObject;
+        GetComponent<Button>().onClick.AddListener(ShowText);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnBecameInvisible() {
+        GetComponent<Image>().enabled = false;
+    }
+
+    private void OnBecameVisible() {
+        GetComponent<Image>().enabled = true;
+    }
+
+    void ShowText() {
+        child.SetActive(!showing);
+        showing = true;
+    }
+
+    public void Deactivate() {
+        child.SetActive(false);
+    }
+
+    private void OnMouseOver() {
+        child.SetActive(true);
+    }
+
+    private void OnMouseExit() {
+        child.SetActive(false);
+    }
+
+    
+    
+}
---
+++ Space Game Prototype/Assets/FirstPersonCameraRotation.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class FirstPersonCameraRotation : MonoBehaviour {
+
+    public Transform target;
+
+	public float Sensitivity {
+		get { return sensitivity; }
+		set { sensitivity = value; }
+	}
+	[Range(0.1f, 9f)][SerializeField] float sensitivity = 2f;
+	[Range(0f, 90f)][SerializeField] float yRotationLimit = 88f;
+
+	Vector2 rotation = Vector2.zero;
+	const string xAxis = "Mouse X";
+	const string yAxis = "Mouse Y";
+
+    void Start() {
+        transform.LookAt(target);
+    }
+
+	void Update(){
+		rotation.x += Input.GetAxis(xAxis) * sensitivity;
+		rotation.y += Input.GetAxis(yAxis) * sensitivity;
+		rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+		var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+		var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+
+		transform.localRotation = xQuat * yQuat;
+    }
+}
